e8cba472f88ef85ec66dd77bb9ad5f905bc66271
Felipe Barcellos <felipe.cellos1@gmail.com>
2026-02-07 16:19:11 -0300

    removendo vue inutilizado

--- app.js
+++
@@ -1,8 +0,0 @@
-// Este arquivo é opcional, mas pode ser útil para separar scripts personalizados no futuro.
-
-new Vue({
-  el: '#app',
-  data: {
-    mensagem: 'Olá, Vue.js funcionando no index.html!'
-  }
-});
